34871ab10d26dea98af7fe2db2ed5e827e734b9c
Alexander Davydov <davydov.al.d@gmail.com>
2017-12-07 22:41:05 +0300

    Beacon recognition logic updated. Proximity settings logic implemented

---
+++ app/src/main/java/com/harman/goodmood/App.java
@@ -0,0 +1,12 @@
+package com.harman.goodmood;
+
+import android.app.Application;
+
+/**
+ * TODO add class description
+ *
+ * @author alexander
+ * @since 07/12/2017
+ */
+public class App extends Application {
+}
---
+++ app/src/main/java/com/harman/goodmood/util/weather/Settings.java
@@ -0,0 +1,10 @@
+package com.harman.goodmood.util.weather;
+
+/**
+ * TODO add class description
+ *
+ * @author alexander
+ * @since 07/12/2017
+ */
+public class Settings {
+}
